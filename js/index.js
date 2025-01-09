7c742279c43f84a3fc1c6d7a3811334469935030
Danielle Guimarães <devdanielleguimaraesoliveira@gmail.com>
2025-01-09 15:30:24 -0300

    Adicionando animação no menu dropdown

--- js/index.js
+++ js/index.js
@@ -20,6 +20,8 @@ function exibeMenu(){
 window.addEventListener("load", ()=>{
     const BTN_NAVEGACAO_TOGGLE = document.getElementById("botao-menu-dropdown");
     const DIV_NAVEGACAO_MENU = document.getElementById("menu-dropdown");
+    const IMG_BOTAO_MENU_TOGGLE = document.getElementById("icone-menu-dropdown");
+    
     
     BTN_NAVEGACAO_TOGGLE.addEventListener ('click', ()=>{
         atualizaEstadoBotaoToggle();
@@ -28,9 +30,9 @@ window.addEventListener("load", ()=>{
 
     function atualizaEstadoBotaoToggle(){
         let estaExpandido = BTN_NAVEGACAO_TOGGLE.getAttribute('aria-expanded') === 'true';
-        const IMG_BOTAO_MENU_TOGGLE = document.getElementById("icone-menu-dropdown");
         
             BTN_NAVEGACAO_TOGGLE.ariaExpanded = !estaExpandido;
+            DIV_NAVEGACAO_MENU.classList.toggle('ativo', !estaExpandido);
             DIV_NAVEGACAO_MENU.classList.toggle('ativo', !estaExpandido);
             IMG_BOTAO_MENU_TOGGLE.src = estaExpandido ? "assets/Mobile/Ícones/icone-menu-verde.png": "assets/Mobile/Ícones/icone-sair-menu.png"
             exibeMenu();
